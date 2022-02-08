8a2b2adaf972f28e55afab5d8b8ecf55a1522766
Gonzalo Gauna <79806700+GonzaloGauna@users.noreply.github.com>
2022-02-07 23:49:17 -0300

    Adding food section:     - Adding styles and animations.     - Responsive ok.     - Animation ok.     - Need to do the review animation.

--- js/app.js
+++ js/app.js
@@ -8,12 +8,34 @@ menu.onclick = () => {
   linksDisplay.classList.toggle('menuActive');
 };
 
-const swiper = new Swiper('.home-swiper', {
-  grabCursor:true,
-  loop:true,
-  centeredSlides:true,
+const homeSwiper = new Swiper('.home-swiper', {
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+});
+
+const foodSwiper = new Swiper('.food-slider', {
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
   },
 });
